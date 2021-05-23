8afd6f4c949e95d0b333721f7c0fae156054180a
Sabri0o <sabritrabelsi90@gmail.com>
2021-05-23 11:40:25 +0100

    serving static files

--- server.js
+++ server.js
@@ -4,6 +4,13 @@ const app = express()
 app.use(express.json())
 app.use(express.urlencoded({extended:true}))
 
+
+app.use('/public',express.static(__dirname + "/public"));
+
+app.get('/',function(req,res){
+    res.sendFile(__dirname + '/views/index.html')
+})
+
 app.listen(3000,()=>{
     console.log("listening on port 3000")
 })

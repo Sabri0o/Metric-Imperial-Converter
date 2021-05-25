c729010f0ad5f1d55422007f540594cdaa22da82
Sabri0o <sabritrabelsi90@gmail.com>
2021-05-25 22:36:20 +0100

    moving to jquery

--- public/client.js
+++ public/client.js
@@ -1,24 +1,21 @@
-document.getElementById("Length").style.display = "block"
-document.getElementById("def").className = document.getElementById("def").className.replace("tablinks"," active")
 
-function openTab(evt, cityName) {
-    document.getElementById("def").className = document.getElementById("def").className.replace(" active","")
-    // Declare all variables
-    var i, tabcontent, tablinks;
-  
-    // Get all elements with class="tabcontent" and hide them
-    tabcontent = document.getElementsByClassName("tabcontent");
-    for (i = 0; i < tabcontent.length; i++) {
-      tabcontent[i].style.display = "none";
+
+ $( document ).ready(function() {
+   $("#Length").show();
+   $("#def").addClass("active");
+   
+   openTab = function (evt, cityName) {
+     $("#def").removeClass("active");
+   
+     // Get all elements with class="tabcontent" and hide them
+     $(".tabcontent").hide();
+   
+     // Get all elements with class="tablinks" and remove the class "active"
+     $(".tablinks").removeClass("active");
+   
+     // Show the current tab, and add an "active" class to the button that opened the tab
+     $(`#${cityName}`).show();
+     evt.currentTarget.className += " active"
     }
-  
-    // Get all elements with class="tablinks" and remove the class "active"
-    tablinks = document.getElementsByClassName("tablinks");
-    for (i = 0; i < tablinks.length; i++) {
-      tablinks[i].className = tablinks[i].className.replace(" active", "");
-    }
-  
-    // Show the current tab, and add an "active" class to the button that opened the tab
-    document.getElementById(cityName).style.display = "block";
-    evt.currentTarget.className += " active"
-}
+  console.log( "ready!" );
+});

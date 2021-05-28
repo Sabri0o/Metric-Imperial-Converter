3ae0a1a8e5d7c5e0fdda3a3f09727572d9a9fe2c
Sabri0o <sabritrabelsi90@gmail.com>
2021-05-28 18:50:43 +0100

    serialize form

--- public/client.js
+++ public/client.js
@@ -1,21 +1,30 @@
+$(document).ready(function () {
+  $("#Length").show();
+  $("#def").addClass("active");
 
+  openTab = function (evt, cityName) {
+    $("#def").removeClass("active");
 
- $( document ).ready(function() {
-   $("#Length").show();
-   $("#def").addClass("active");
-   
-   openTab = function (evt, cityName) {
-     $("#def").removeClass("active");
-   
-     // Get all elements with class="tabcontent" and hide them
-     $(".tabcontent").hide();
-   
-     // Get all elements with class="tablinks" and remove the class "active"
-     $(".tablinks").removeClass("active");
-   
-     // Show the current tab, and add an "active" class to the button that opened the tab
-     $(`#${cityName}`).show();
-     evt.currentTarget.className += " active"
-    }
-  console.log( "ready!" );
+    // Get all elements with class="tabcontent" and hide them
+    $(".tabcontent").hide();
+
+    // Get all elements with class="tablinks" and remove the class "active"
+    $(".tablinks").removeClass("active");
+
+    // Show the current tab, and add an "active" class to the button that opened the tab
+    $(`#${cityName}`).show();
+    evt.target.className += " active";
+  };
+
+  // $('#lengthConverter').submit(function(evt){
+  //   evt.preventDefault()
+  //   console.log($('#lengthConverter').serialize())
+  // })
+
+  $( "form" ).on( "submit", function( event ) {
+    event.preventDefault();
+    console.log( $( this ).serialize() );
+  });
+
+  console.log("ready!");
 });
